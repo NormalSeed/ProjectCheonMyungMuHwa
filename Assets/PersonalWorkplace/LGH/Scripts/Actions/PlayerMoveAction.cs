9d387f69c1be0965f8eff96a17005a1720460e47
NormalSeed <dlrjsghk11@naver.com>
2025-08-21 14:24:46 +0900

    [Fix] Player Move Action
    플레이어의 이동 포인트 지정 로직 수정

--- Assets/PersonalWorkplace/LGH/Scripts/Actions/PlayerMoveAction.cs
+++ Assets/PersonalWorkplace/LGH/Scripts/Actions/PlayerMoveAction.cs
@@ -8,26 +8,25 @@ using System.Linq;
 using UnityEngine.AI;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "PlayerMove", story: "[Self] move to [AlignPoints]", category: "Action", id: "ac6be75cade22ceed3bfae9a3e9af1c2")]
+[NodeDescription(name: "PlayerMove", story: "[Self] move to [AlignPoint]", category: "Action", id: "ac6be75cade22ceed3bfae9a3e9af1c2")]
 public partial class PlayerMoveAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> Self;
-    [SerializeReference] public BlackboardVariable<List<GameObject>> AlignPoints;
+    [SerializeReference] public BlackboardVariable<GameObject> AlignPoint;
 
     private NavMeshAgent NMagent;
+    private AlignPoint alignPoint;
 
     protected override Status OnStart()
     {
         NMagent = Self.Value.GetComponent<NavMeshAgent>();
 
-        // AlignPoint 태그를 가진 이동 포인트를 검색해서
-        GameObject[] foundPoints = GameObject.FindGameObjectsWithTag("AlignPoint");
-        // AlignPoints 리스트에 넣어줌
-        AlignPoints.Value = foundPoints.ToList();
+        AlignPoint.Value = GameObject.FindGameObjectWithTag("AlignPoint");
+        alignPoint = AlignPoint.Value.GetComponent<AlignPoint>();
 
         // 자신의 배치 번호에 따라 이동 포인트로 이동하는 기능 구현 필요
         // 이동 포인트의 position을 NavMeshAgent의 목적지로 설정
-        NMagent.SetDestination(GetMovePoint(AlignPoints.Value).position); // 배치 번호를 바꾸면 이동할 포인트도 변경됨
+        NMagent.SetDestination(GetMovePoint(alignPoint.alignPoints).position); // 배치 번호를 바꾸면 이동할 포인트도 변경됨
 
         return Status.Running;
     }
@@ -35,12 +34,15 @@ public partial class PlayerMoveAction : Action
     private Transform GetMovePoint(List<GameObject> points)
     {
         // 배치 번호에 따라 이동 포인트로 이동하는 로직 구현 필요
-        return points[0].transform;
+        Transform point = points[0].transform;
+
+        Debug.Log($"이동 포인트 : {point.gameObject.name}");
+        return point;
     }
 
     protected override Status OnUpdate()
     {
-        if (Vector2.Distance(Self.Value.transform.position, GetMovePoint(AlignPoints.Value).position) < 0.1f)
+        if (Vector2.Distance(Self.Value.transform.position, GetMovePoint(alignPoint.alignPoints).position) < 0.1f)
         {
             NMagent.ResetPath();
             return Status.Success;
