9321eeaa052911689b5ee4c57e40745e5493f419
KyuSSeo <kyuri2665@gmail.com>
2025-08-22 16:24:55 +0900

    [Feat] HeroSet btn

--- Assets/PersonalWorkplace/MSK/MSK_Scripts/UI/HeroUI.cs
+++ Assets/PersonalWorkplace/MSK/MSK_Scripts/UI/HeroUI.cs
@@ -6,9 +6,13 @@ public class HeroUI : UIBase
     [Header("Buttons")]
     [SerializeField] private Button stageUpgrade;
     [SerializeField] private Button heroSet;
+    [SerializeField] private Button autoSet;
     [SerializeField] private Button heroSetSave;
 
+    [Header("Root References")]
+    [SerializeField] private Transform IsHeroSetting;
 
+    public bool IsHeroSetNow = false;
 
     #region Unity LifeCycle
     private void OnEnable()
@@ -24,23 +28,54 @@ public class HeroUI : UIBase
     }
     #endregion
 
+
+
     #region Button OnClick
+    
+    //  자동 승급
     private void onClickStageUpgrade()
     {
         stageUpgrade.gameObject.SetActive(false);
         // 승급 완료 후 버튼 비활성화
         stageUpgrade.onClick.RemoveListener(onClickStageUpgrade);
     }
+
+
+    //  영웅 배치 시작
     private void OnClickHeroSet()
     {
-        // 영웅 배치에 변경이 있을 경우에만 활성화
-        //heroSetSave.onClick.AddListener(OnClickHeroSetSave);
+        //  버튼 비활성화
+        heroSet.gameObject.SetActive(false);
+
+        //  영웅 편성화면 활성화
+        IsHeroSetting.gameObject.SetActive(true);
+        heroSetSave.gameObject.SetActive(true);
+        autoSet.gameObject.SetActive(true);
+
+        heroSetSave.onClick.AddListener(OnClickHeroSetSave);
+        autoSet.onClick.AddListener(OnClickAutoSet);
     }
+
+    //  영웅 자동 배치
+    private void OnClickAutoSet()
+    {
+        // 규칙에 따라서 영웅을 자동으로 배치
+        // 
+    }
+
+    //  영웅 배치 저장
     private void OnClickHeroSetSave()
     {
-        //  변경 후 버튼 비활성화
+        //  변경 후 비활성화
+        IsHeroSetting.gameObject.SetActive(false);
         heroSetSave.gameObject.SetActive(false);
+        autoSet.gameObject.SetActive(false);
+
         heroSetSave.onClick.RemoveListener(OnClickHeroSetSave);
+        autoSet.onClick.RemoveListener(OnClickAutoSet);
+
+        // 배치라기 버튼 활성화
+        heroSet.gameObject.SetActive(true);
     }
     #endregion
 }
