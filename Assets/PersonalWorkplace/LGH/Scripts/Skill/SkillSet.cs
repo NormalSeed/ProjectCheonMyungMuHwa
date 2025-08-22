a79283e1925b921654c20323b402554c2d5a7964
NormalSeed <dlrjsghk11@naver.com>
2025-08-22 12:08:29 +0900

    [Feat] SkillSet class field
    스킬셋에 관련 필드 작성

---
+++ Assets/PersonalWorkplace/LGH/Scripts/Skill/SkillSet.cs
@@ -0,0 +1,16 @@
+using Unity.Collections;
+using UnityEngine;
+
+public class SkillSet : MonoBehaviour
+{
+    [SerializeField] public string SkillID;
+    [SerializeField] public string Skillname;
+    [SerializeField] public string SkillType;
+    [SerializeField] public string EffectType;
+    [SerializeField] public float ExtSkillDmg;
+    [SerializeField] public float InnSkillDmg;
+    [SerializeField] public float CoolTime;
+    [SerializeField] public float SkillRange;
+
+
+}
