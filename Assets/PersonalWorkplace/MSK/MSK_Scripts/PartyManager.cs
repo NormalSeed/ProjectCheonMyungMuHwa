afe6a45c4ce895d443b81b6d592b822628623f88
KyuSSeo <kyuri2665@gmail.com>
2025-08-25 17:29:10 +0900

    [Feat] Day End Commit
    오후 회의 진행 전 커밋
    
    파티 시스템 관련, PartyInit 함수를 통한 파티 멤버 설정을 만들고자 합니다.

--- Assets/PersonalWorkplace/MSK/MSK_Scripts/PartyManager.cs
+++ Assets/PersonalWorkplace/MSK/MSK_Scripts/PartyManager.cs
@@ -6,6 +6,12 @@ public class PartyManager : MonoBehaviour
     public List<GameObject> partyMembers = new List<GameObject>();
     private readonly int MaxPartySize = 5;
 
+    #region Unity LifeCycle
+    private void Start()
+    {
+        PartyInit();
+    }
+    #endregion
 
     #region Public 
     public void AddMember(GameObject member)
@@ -23,12 +29,22 @@ public class PartyManager : MonoBehaviour
             partyMembers.Remove(member);
         }
     }
+    public void PartyInit()
+    {
+        foreach (var partyMembers in partyMembers)
+        {
+            // 파티 멤버 초기화
+        }
+    }
     #endregion
 
     #region Private
-    private void CheakSynergy()
+    private void CheakSynergy( )
     {
+        foreach (var member in partyMembers)
+        {
 
+        }
     }
     #endregion
 }
