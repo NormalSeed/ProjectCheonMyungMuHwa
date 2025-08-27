f8ceb2054b2a032dce7a2b31f0ebb71a55ca7666
NormalSeed <dlrjsghk11@naver.com>
2025-08-27 21:13:28 +0900

    [Fix] Build Settings

--- Assets/PersonalWorkplace/KMS/Script/BossController.cs
+++ Assets/PersonalWorkplace/KMS/Script/BossController.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Collections;
-using NUnit.Framework.Internal;
 using Unity.Behavior;
-using Unity.Mathematics;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.AI;
 
