b4eba7691de1c5ab46fed50ff87b296c707bdca2
munsung143 <akunta3475@gmail.com>
2025-09-02 18:37:24 +0900

    [Fix] 몬스터 소환함수 수정

--- Assets/PersonalWorkplace/KMS/Script/PoolManager.cs
+++ Assets/PersonalWorkplace/KMS/Script/PoolManager.cs
@@ -10,7 +10,7 @@ public class PoolManager : MonoBehaviour
     public static PoolManager Instance;
     public DefaultPool<MonsterController> PunchPool;
     public DefaultPool<MonsterController> StickPool;
-    public DefaultPool<MonsterController> CainPool;
+    public DefaultPool<MonsterController> CanePool;
     public DefaultPool<MonsterController> BowPool;
 
     public DefaultPool<MonsterProjectile> ArrowPool;
@@ -20,7 +20,7 @@ public class PoolManager : MonoBehaviour
 
     public DefaultPool<DroppedItem> ItemPool;
 
-    [SerializeField] private Transform[] spawwnPoints;
+    [SerializeField] private Transform[] spawnPoints;
     [SerializeField] private Transform bossPoint;
 
     int currentstage;
@@ -53,7 +53,7 @@ public class PoolManager : MonoBehaviour
         bosses = new();
         PunchPool = new DefaultPool<MonsterController>("Punch", 3, false);
         StickPool = new DefaultPool<MonsterController>("Stick", 3, false);
-        CainPool = new DefaultPool<MonsterController>("Cane", 3, false);
+        CanePool = new DefaultPool<MonsterController>("Cane", 3, false);
         BowPool = new DefaultPool<MonsterController>("Bow", 3, false);
         ArrowPool = new DefaultPool<MonsterProjectile>("Arrow", 8);
         MagicPool = new DefaultPool<MonsterProjectile>("MagicBall", 8);
@@ -70,31 +70,59 @@ public class PoolManager : MonoBehaviour
             boss.SetActive(false);
         }
     }
-
-
-    public void ActiveAll(int stageNum)
+    //현재 스테이지에 따른 몬스터, 보스 스텟 설정 (1~1200)
+    public void SetMonsterState(int stage)
     {
-        currentstage = stageNum;
-        AudioManager.Instance.PlaySound("Monster_Recall_New");
+        currentstage = stage;
         SetModelStates();
-
-        ActiveMonster(PunchPool, 0, 0);
-        ActiveMonster(PunchPool, 1, 0);
-        ActiveMonster(PunchPool, 2, 0);
-        ActiveMonster(StickPool, 3, 1);
-        ActiveMonster(StickPool, 4, 1);
-        ActiveMonster(StickPool, 5, 1);
-        ActiveMonster(CainPool, 6, 2);
-        ActiveMonster(CainPool, 7, 2);
-        ActiveMonster(CainPool, 8, 2);
-        ActiveMonster(BowPool, 9, 3);
-        ActiveMonster(BowPool, 10, 3);
-        ActiveMonster(BowPool, 11, 3);
+        bossModel.SetFinalBoss(currentstage, models[0]);
     }
 
-    public void ActiveBoss(int stageNum) //3의 배수로만 들어오는 것을 상정함
+    //지정된 위치에 몬스터 소환
+    public void SpawnMonster(Vector2 pos, MonsterType type)
     {
-        currentstage = stageNum;
+        if (type == MonsterType.Punch)
+        {
+            ActiveMonster(PunchPool, 0, pos);
+        }
+        else if (type == MonsterType.Stick)
+        {
+            ActiveMonster(StickPool, 1, pos);
+        }
+        else if (type == MonsterType.Cane)
+        {
+            ActiveMonster(CanePool, 2, pos);
+        }
+        else if (type == MonsterType.Bow)
+        {
+            ActiveMonster(BowPool, 3, pos);
+        }
+        else if (type == MonsterType.Boss)
+        {
+            ActiveBoss(pos);
+        }
+    }
+
+    public void ActiveAll(int stageNum) // 기존에 사용하던 몬스터 소환 함수 (사용 X)
+    {
+        SetMonsterState(stageNum);
+        ActiveMonster(PunchPool, 0, spawnPoints[0].position);
+        ActiveMonster(PunchPool, 0, spawnPoints[1].position);
+        ActiveMonster(PunchPool, 0, spawnPoints[2].position);
+        ActiveMonster(StickPool, 1, spawnPoints[3].position);
+        ActiveMonster(StickPool, 1, spawnPoints[4].position);
+        ActiveMonster(StickPool, 1, spawnPoints[5].position);
+        ActiveMonster(CanePool, 2, spawnPoints[6].position);
+        ActiveMonster(CanePool, 2, spawnPoints[7].position);
+        ActiveMonster(CanePool, 2, spawnPoints[8].position);
+        ActiveMonster(BowPool, 3, spawnPoints[9].position);
+        ActiveMonster(BowPool, 3, spawnPoints[10].position);
+        ActiveMonster(BowPool, 3, spawnPoints[11].position);
+    }
+
+    private void ActiveBoss(Vector2 pos)
+    {
+        AudioManager.Instance.PlaySound("Monster_Recall_New");
         int last = currentstage / 3 % 10;
         string str;
         switch (last)
@@ -107,27 +135,20 @@ public class PoolManager : MonoBehaviour
             default: str = ""; break;
 
         }
-        AudioManager.Instance.PlaySound("Monster_Recall_New");
-        bossModel.SetFinalBoss(currentstage, models[0]);
-
         MonsterController bosscon = bosses[str];
-        bosscon.transform.position = bossPoint.position;
+        bosscon.transform.position = pos;
         bosscon.Model.BaseModel = bossModel;
         bosscon.gameObject.SetActive(true);
-
     }
-    private void ActiveMonster(DefaultPool<MonsterController> pool, int pointIndex, int modelIndex)
+    public void ActiveBoss(int dummy) //컴파일 오류 방지용 오버로딩
     {
-        if (currentstage < 601)
-        {
-            ParticleManager.Instance.GetParticle("M_12_Recall", spawwnPoints[pointIndex].position);
-        }
-        else
-        {
-            ParticleManager.Instance.GetParticle("M_34_Recall", spawwnPoints[pointIndex].position);
-        }
-
-        MonsterController monster = pool.GetItem(spawwnPoints[pointIndex].position);
+        
+        ActiveBoss(bossPoint.position);
+    }
+    private void ActiveMonster(DefaultPool<MonsterController> pool, int modelIndex, Vector2 pos)
+    {
+        AudioManager.Instance.PlaySound("Monster_Recall_New");
+        MonsterController monster = pool.GetItem(pos);
         monster.Model.InitSprite(currentstage);
         monster.Model.BaseModel = models[modelIndex];
         monster.gameObject.SetActive(true);
@@ -162,11 +183,5 @@ public class PoolManager : MonoBehaviour
 
     }
 
-    public void MoveTo(Vector2 pos)
-    {
-        transform.position = pos;
-    }
-
-
-
 }
+public enum MonsterType { Punch, Stick, Cane, Bow, Boss }
