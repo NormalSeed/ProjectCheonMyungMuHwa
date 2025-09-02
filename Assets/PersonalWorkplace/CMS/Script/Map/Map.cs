c99a4ff14773ce4e0c42ee2bba05d1c807458e49
NormalSeed <dlrjsghk11@naver.com>
2025-09-03 04:15:26 +0900

    [Feat] Player Move to Next Stage
    플레이어가 몬스터를 모두 처리한 후 다음으로 소환된 맵의 정렬 포인트로 이동하는 기능 구현

--- Assets/PersonalWorkplace/CMS/Script/Map/Map.cs
+++ Assets/PersonalWorkplace/CMS/Script/Map/Map.cs
@@ -4,27 +4,30 @@ public class Map : MonoBehaviour
 {
     public Transform endPoint;
     public int totalEnemies;
-    private int defeatedEnemies = 0;
-    private bool cleared = false; // 중복 방지
 
-    public void OnEnemyDefeated()
-    {
-        defeatedEnemies++;
+    public bool IsCleared => InGameManager.Instance.monsterDeathStack.Value <= 0;
 
-        if (!cleared && IsCleared())
-        {
-            cleared = true; // 딱 한 번만 실행
-            Debug.Log("맵 클리어! 다음 스테이지 생성");
+    //private int defeatedEnemies = 0;
+    //private bool cleared = false; // 중복 방지
 
-            if (MapManager.Instance != null)
-            {
-                MapManager.Instance.GoToNextStage(endPoint.position);
-            }
-        }
-    }
+    //public void OnEnemyDefeated()
+    //{
+    //    defeatedEnemies++;
 
-    public bool IsCleared()
-    {
-        return defeatedEnemies >= totalEnemies;
-    }
+    //    if (!cleared && IsCleared())
+    //    {
+    //        cleared = true; // 딱 한 번만 실행
+    //        Debug.Log("맵 클리어! 다음 스테이지 생성");
+
+    //        if (MapManager.Instance != null)
+    //        {
+    //            MapManager.Instance.GoToNextStage(endPoint.position);
+    //        }
+    //    }
+    //}
+
+    //public bool IsCleared()
+    //{
+    //    return defeatedEnemies >= totalEnemies;
+    //}
 }
