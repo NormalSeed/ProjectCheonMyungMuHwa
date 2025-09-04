6d0fcec30374990eb8f7f5a2f484275256e3f54e
frhug922 <dlsrnjsl3@gmail.com>
2025-09-04 16:59:48 +0900

    [Feat] Add PopupManager, PopupCanvas
    팝업을 담당하는 PopupCanvas 추가 및 PopupManager 추가

---
+++ Assets/PersonalWorkplace/BIK/Scripts/UI/PopupManager.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PopupEntry
+{
+    public PopupType popupType;
+    public UIBase uiBase;
+}
+
+public class PopupManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static PopupManager Instance { get; private set; }
+
+    #endregion // Singleton
+
+
+
+
+
+    #region serialized fields
+
+    [Header("Popup Panel")]
+    [SerializeField] private List<PopupEntry> _popupEntries = new List<PopupEntry>();
+
+    #endregion // serialized fields
+
+
+
+
+
+    #region private fields
+
+    private Dictionary<PopupType, UIBase> _popupDict;
+
+    #endregion // private fields
+
+
+
+
+
+    #region mono funcs
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        // Dictionary 변환
+        _popupDict = new Dictionary<PopupType, UIBase>();
+        foreach (var entry in _popupEntries) {
+            if (!_popupDict.ContainsKey(entry.popupType))
+                _popupDict.Add(entry.popupType, entry.uiBase);
+        }
+    }
+
+    #endregion // mono funcs
+
+
+
+
+
+    #region public funcs
+
+    public void ShowPopup(PopupType popupType, string message, Action onLeft = null, Action onRight = null)
+    {
+        if (!_popupDict.TryGetValue(popupType, out var popup) || popup == null) {
+            Debug.LogWarning($"[PopupManager] 등록되지 않은 팝업: {popupType}");
+            return;
+        }
+
+        popup.SetShow();
+        Debug.Log($"[PopupManager] ShowPopup: {popupType}, Message: {message}");
+
+        // TODO: popup 내부에 message, 버튼 콜백 전달
+    }
+
+    public void ClosePopup(PopupType popupType)
+    {
+        if (_popupDict.TryGetValue(popupType, out var popup) && popup != null) {
+            popup.SetHide();
+            Debug.Log($"[PopupManager] ClosePopup: {popupType}");
+        }
+    }
+
+    public void CloseAllPopups()
+    {
+        foreach (var kvp in _popupDict) {
+            kvp.Value?.SetHide();
+        }
+        Debug.Log("[PopupManager] 모든 팝업 닫음");
+    }
+
+    #endregion // public funcs
+}
