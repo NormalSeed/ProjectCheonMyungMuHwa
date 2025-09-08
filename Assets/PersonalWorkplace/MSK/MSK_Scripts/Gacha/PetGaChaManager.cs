16d084c4bbd0d17ec7263a93ba13b8ccae9f8de8
KyuSSeo <kyuri2665@gmail.com>
2025-09-08 16:29:44 +0900

    [Feat] Gacha Scripts
    각각의 가챠 스크립트 생성

---
+++ Assets/PersonalWorkplace/MSK/MSK_Scripts/Gacha/PetGaChaManager.cs
@@ -0,0 +1,226 @@
+using Firebase.Auth;
+using Firebase.Database;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class PetGachaManager : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private SummonResultUI resultUI;
+
+    #region FireBase Properties
+    private string _uid;
+    private DatabaseReference _dbRef;
+
+    private float summonNormal;             // 노말 소환확률           
+    private float summonRare;               // 레어 소환확률
+    private float summonUnique;             // 유니트 소환확률
+    private float summonEpic;               // 에픽 소환확률
+    private SummonLevel userSummonLevel;    // 소환 레벨
+    private int userSummonCount;            // 유저의 소환 레벨업 수치 
+    private int requireSummonCount;         // 소환 레벨업 요구량
+
+    private Dictionary<string, List<string>> rarityHeroMap = new(); // 레어도 별 영웅 리스트
+    #endregion
+    
+    private Dictionary<string, CardInfo> cardInfoCache = new(); // 캐싱용 딕셔너리
+    // 가중치 풀
+    private Dictionary<string, float> GetWeightPool()
+    {
+        return new Dictionary<string, float>
+        {
+            { "Normal", summonNormal },
+            { "Rare", summonRare },
+            { "Unique", summonUnique },
+            { "Epic", summonEpic }
+        };
+    }
+
+    #region Unity LifeCycle
+
+    private async void OnEnable()
+    {
+        _uid = CurrencyManager.Instance.UserID;
+        _dbRef = CurrencyManager.Instance.DbRef;
+
+        await LoadUserDataAsync();
+        await LoadSummonConfigAsync();
+    }
+    private void OnDisable() { }
+    #endregion
+
+    #region Private
+
+    //  소환 레벨 별 확률정보 로딩
+    private async Task LoadSummonConfigAsync()
+    {
+        var summonSnap = await _dbRef.Child("summon").GetValueAsync();
+
+        // 확률 및 요구 수치 로딩
+        var configSnap = summonSnap.Child(userSummonLevel.ToString());
+        summonNormal = Convert.ToSingle(configSnap.Child("normal").Value);
+        summonRare = Convert.ToSingle(configSnap.Child("rare").Value);
+        summonUnique = Convert.ToSingle(configSnap.Child("unique").Value);
+        summonEpic = Convert.ToSingle(configSnap.Child("epic").Value);
+        requireSummonCount = Convert.ToInt32(configSnap.Child("count").Value);
+
+        // 영웅 리스트 로딩
+        var heroListSnap = summonSnap.Child("heroList");
+        rarityHeroMap.Clear();
+
+        foreach (var rarityNode in heroListSnap.Children)
+        {
+            rarityHeroMap[rarityNode.Key.ToLower()] = rarityNode.Children
+                .Select(hero => hero.Key)
+                .ToList();
+        }
+    }
+    // 유저 뽑기정보 로딩
+    private async Task LoadUserDataAsync()
+    {
+        var snapProfile = await CurrencyManager.Instance.LoadUserProfileAsync();
+        userSummonLevel = snapProfile.SummonLevel;
+        userSummonCount = snapProfile.SummonCount;
+    }
+    private Task SaveUserSummonLevelAsync()
+    {
+        return _dbRef.Child("users").Child(_uid).Child("profile").Child("summonLevel").SetValueAsync((int)userSummonLevel);
+    }
+
+    private Task SaveUserSummonCountAsync()
+    {
+        return _dbRef.Child("users").Child(_uid).Child("profile").Child("summonCount").SetValueAsync(userSummonCount);
+    }
+
+    // 카드 정보 표기
+    private async Task<CardInfo> LoadCardInfoByCode(string heroCode)
+    {
+        if (cardInfoCache.TryGetValue(heroCode, out var cached))
+            return cached;
+
+        string addressKey = $"{heroCode}CardInfo";
+        var handle = Addressables.LoadAssetAsync<CardInfo>(addressKey);
+        await handle.Task;
+
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            cardInfoCache[heroCode] = handle.Result;
+            return handle.Result;
+        }
+
+        Debug.LogError($"[{nameof(LoadCardInfoByCode)}] {addressKey} 로드 실패");
+        return null;
+    }
+
+    private async Task<CardInfo> LoadCardInfoByRarity(string rarity)
+    {
+        string key = rarity.ToLower();
+        if (!rarityHeroMap.TryGetValue(key, out var heroList) || heroList.Count == 0)
+        {
+            Debug.LogError($"[{nameof(LoadCardInfoByRarity)}] {rarity}에 해당하는 영웅 리스트 없음");
+            return null;
+        }
+
+        string selectedCode = heroList[UnityEngine.Random.Range(0, heroList.Count)];
+        return await LoadCardInfoByCode(selectedCode);
+    }
+
+
+    // 실제 소환 코드
+    public async Task Summon(int times)
+    {
+        userSummonCount += times;
+        if (userSummonCount >= requireSummonCount)
+        {
+            await LevelUpAsync();
+        }
+
+        var results = new List<CardInfo>();
+        for (int i = 0; i < times; i++)
+        {
+            string rarityKey = DrawRarity();
+            CardInfo info = await LoadCardInfoByRarity(rarityKey);
+            results.Add(info);
+        }
+
+        StartCoroutine(ProcessResultsCoroutine(results));
+        await SaveUserSummonCountAsync();
+        resultUI.ShowSummonResult(results);
+        // await SaveGachaHistoryAsync(results); //소환기록 추가
+    }
+
+    //  소환레벨 업
+    private async Task LevelUpAsync()
+    {
+        // 1~9까지만 처리
+        if (userSummonLevel < SummonLevel.level10)
+        {
+            userSummonLevel = (SummonLevel)((int)userSummonLevel + 1);
+            userSummonCount = 0;
+
+            await SaveUserSummonLevelAsync();
+            await LoadSummonConfigAsync();
+        }
+    }
+
+    //  소환 확률 결정
+    private string DrawRarity()
+    {
+        var weightPool = GetWeightPool();
+        float sum = weightPool.Values.Sum();
+        float random = UnityEngine.Random.Range(0f, sum);
+
+        foreach (var keyValue in weightPool)
+        {
+            if (random <= keyValue.Value) return keyValue.Key;
+            random -= keyValue.Value;
+        }
+        return weightPool.Keys.Last();
+    }
+
+    //  소환기록 저장하기
+    private async Task SaveGachaHistoryAsync(List<CardInfo> results)
+    {
+        var historyRef = _dbRef.Child("users").Child(_uid).Child("summonHistory");
+
+        foreach (var card in results)
+        {
+            string key = historyRef.Push().Key;
+
+            var data = new Dictionary<string, object>
+        {
+            { "timestamp", ServerValue.Timestamp },
+            { "heroID", card.HeroID },
+
+        };
+
+            await historyRef.Child(key).SetValueAsync(data);
+            // 기록 삭제
+            // await historyRef.RemoveValueAsync();
+        }
+    }
+    // 영웅 조각 정보를 업로드하는 코루틴
+    private IEnumerator ProcessResultsCoroutine(List<CardInfo> results)
+    {
+        // HeroDataManager에 결과 반영
+        HeroDataManager.Instance.ApplyGachaResults(results);
+
+        // 한번에 저장
+        HeroDataManager.Instance.SaveHeroDataToCache();
+        HeroDataManager.Instance.SaveAllHeroDataToFirebase();
+
+        yield return null;
+        Debug.Log("영웅 뽑기 결과 처리 완료"); 
+    }
+    #endregion
+}
+/*
+    TODO : 뽑기 해야할 일 목록
+        영웅 뽑기 확률, 리스트 풀 수정하기
+ */
---
+++ Assets/PersonalWorkplace/MSK/MSK_Scripts/UI/SummonPetUI.cs
@@ -0,0 +1,142 @@
+using TMPro;
+using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+
+public class SummonPetUI : MonoBehaviour
+{
+    #region SerializeField
+    [Header("Buttons")]
+    [SerializeField] private Button summonButton;           // 단챠
+    [SerializeField] private Button summon10thButton;       // 10챠
+    [SerializeField] private Button summon50thTimesButton;  // 50챠
+    [SerializeField] private Button summonInfo;             // 확률정보
+    [SerializeField] private Button summonResult;
+
+    [Header("Text")]
+    [SerializeField] private TextMeshProUGUI summonLevelText;   // 소환레벨 텍스트
+    [SerializeField] private TextMeshProUGUI button1Text;       // 단챠 버튼 텍스트
+    [SerializeField] private TextMeshProUGUI button10Text;      // 10챠 버튼 텍스트
+    [SerializeField] private TextMeshProUGUI button50Text;      // 50챠 버튼 텍스트
+
+    [Header("Hero Image")]
+    [SerializeField] private Transform Hero1;                   // 영웅 이미지
+    [SerializeField] private Transform Hero2;                   // 영웅 이미지
+    [SerializeField] private Transform Machine;                 // 뽑기 이미지
+    [SerializeField] private Transform Equipment;               // 장비 이미지
+
+    [Header("Panel")]
+    [SerializeField] private SummonResultUI summonResultUI;    // 소환 결과창
+    [SerializeField] private GachaManager gachaManager;     // 가챠 메니저
+    [SerializeField] private GameObject summonInfoPanel;    // 소환확률 정보창
+
+    [Header("Slider")]
+    [SerializeField] private Slider summonSlider;           // 소환래벨 슬라이더
+
+    #endregion
+
+    #region Properties
+    private int summonCount;
+    private int requireCount;
+    private SummonLevel userSummonLevel;
+    #endregion
+
+    #region Unity LifeCycle
+    private void OnEnable()
+    {
+        Init();
+
+    }
+
+    private void OnDisable()
+    {
+        summonButton.onClick.RemoveListener(onClickSummon);
+        summon10thButton.onClick.RemoveListener(onClickSummon10th);
+        summon50thTimesButton.onClick.RemoveListener(onClickSummon50th);
+        summonInfo.onClick.RemoveListener(OnClickShowInfo);
+
+        summonResultUI.OnGachaCompleted -= HandleGachaCompleted;
+    }
+
+    private void Init()
+    {
+        ButtonInit();
+        SummonLevelChange();
+        summonResultUI.OnGachaCompleted += HandleGachaCompleted;
+    }
+
+    private void ButtonInit()
+    {
+        summonInfo.onClick.AddListener(OnClickShowInfo);
+        summonButton.onClick.AddListener(onClickSummon);
+        summon10thButton.onClick.AddListener(onClickSummon10th);
+        summon50thTimesButton.onClick.AddListener(onClickSummon50th);
+    }
+    #endregion
+
+    #region Button OnClick
+    private void onClickSummon()
+    {
+        SummonHeros(1);
+        InterActButtons(false);
+    }
+    private void onClickSummon10th()
+    {
+        SummonHeros(10);
+        InterActButtons(false);
+    }
+    private void onClickSummon50th()
+    {
+        SummonHeros(50);
+        InterActButtons(false);
+    }
+    private void OnClickShowInfo()
+    {
+        summonInfoPanel.SetActive(true);
+    }
+    #endregion
+
+    #region private
+
+    private void InterActButtons(bool input)
+    {
+        summonButton.interactable = input;
+        summon10thButton.interactable = input;
+        summon50thTimesButton.interactable = input;
+        summonInfo.interactable = input;
+        summonResult.interactable = input;
+    }
+    private async Task SummonLevelChange()
+    {
+        var profile = await CurrencyManager.Instance.LoadUserProfileAsync();
+        summonCount = profile.SummonCount;
+        userSummonLevel = profile.SummonLevel;
+        requireCount = await CurrencyManager.Instance.LoadRequireCountFromFireBase(userSummonLevel.ToString());
+
+        summonLevelText.text = "장비 뽑기 레벨 " + ((int)userSummonLevel).ToString();
+        UpdateSummonSlider();
+    }
+    private void UpdateSummonSlider()
+    {
+        summonSlider.maxValue = requireCount;
+        summonSlider.value = summonCount;
+    }
+    private async Task SummonHeros(int times)
+    {
+        summonResultUI.gameObject.SetActive(true);
+        await gachaManager.Summon(times);
+    }
+
+    private void HandleGachaCompleted()
+    {
+        SummonLevelChange();
+        InterActButtons(true);
+    }
+
+    private void ChangeButtonText()
+    {
+        /*   TODO : 가진 재화를 확인하여 소환 타입을 설정하기   */
+    }
+    #endregion
+}
+
