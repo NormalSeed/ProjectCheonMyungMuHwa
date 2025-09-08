52c62a33abfba797322c02293fdff829f80c1761
NormalSeed <dlrjsghk11@naver.com>
2025-09-08 09:22:27 +0900

    [Feat] Init Equipping equipment

--- Assets/PersonalWorkplace/LGH/Scripts/Equipment/CharacterEquipment.cs
+++ Assets/PersonalWorkplace/LGH/Scripts/Equipment/CharacterEquipment.cs
@@ -13,9 +13,18 @@ public class CharacterEquipment : MonoBehaviour
     [Inject]
     private EquipmentService equipmentService;
 
+    private PlayerController controller;
+
     public string charID;
     public EquipClass equipClass;
     public Dictionary<EquipmentType, EquipmentSlot> slots = new();
+
+    private void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        charID = controller.model.modelSO.CharID;
+        equipClass = GetEquipClass();
+    }
 
     private void OnEnable()
     {
@@ -23,5 +32,18 @@ public class CharacterEquipment : MonoBehaviour
         equipmentService.RegisterCharacter(this);
     }
 
-    
+    private EquipClass GetEquipClass()
+    {
+        switch (controller.model.modelSO.Position)
+        {
+            case 1:
+                return EquipClass.Front;
+            case 2:
+                return EquipClass.Middle;
+            case 3:
+                return EquipClass.Back;
+            default:
+                return EquipClass.Front;
+        }
+    }
 }
