b3ddb0f1323d87adfab63cf90f01ec9230b3b368
KyuSSeo <kyuri2665@gmail.com>
2025-09-17 17:11:23 +0900

    [Feat] Rate Sctipts
    소환확률 스크립트 초기작성

---
+++ Assets/PersonalWorkplace/MSK/MSK_Scripts/Gacha/GachaRateInfoUI.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GachaRateInfoUI : MonoBehaviour
+{
+    [Header("Button")]
+    [SerializeField] private Button exitButton;
+    [SerializeField] private Button lv1Button;
+    [SerializeField] private Button lv2Button;
+    [SerializeField] private Button lv3Button;
+    [SerializeField] private Button lv4Button;
+    [SerializeField] private Button lv5Button;
+    [SerializeField] private Button lv6Button;
+    [SerializeField] private Button lv7Button;
+    [SerializeField] private Button lv8Button;
+    [SerializeField] private Button lv9Button;
+    [SerializeField] private Button lv10Button;
+
+    [Header("Text")]
+    [SerializeField] private TextMeshProUGUI nRateText;
+    [SerializeField] private TextMeshProUGUI rRateText;
+    [SerializeField] private TextMeshProUGUI uRateText;
+    [SerializeField] private TextMeshProUGUI lRateText;
+
+    [Header("Panel")]
+    [SerializeField] private GameObject legendPanel;
+
+    #region Unity
+    private void OnEnable()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        
+    }
+    #endregion
+
+    #region OnClick
+    #endregion
+
+    #region Private
+    #endregion
+}
