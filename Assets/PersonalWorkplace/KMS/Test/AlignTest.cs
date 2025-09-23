38319848be092a25a3f5f9fbcf89ad181855254e
munsung143 <akunta3475@gmail.com>
2025-09-23 11:33:25 +0900

    [feat] 실패, 룰렛 UI제작

--- Assets/PersonalWorkplace/KMS/Test/AlignTest.cs
+++ Assets/PersonalWorkplace/KMS/Test/AlignTest.cs
@@ -5,37 +5,85 @@ using UnityEngine.UI;
 public class AlignTest : MonoBehaviour
 {
 
-    [SerializeField] RectTransform canversTrs;
-    [SerializeField] RectTransform contentsTrs;
+    [SerializeField] bool clockwise;
+    [SerializeField] float defaultDPS;
+    [SerializeField] Transform roulette;
+    [SerializeField] float targetDeg;
+    [SerializeField] int samplingCount;
 
-    [SerializeField] GameObject[] cards;
-    [SerializeField] Scrollbar scrollbar;
-
-
-    private List<RectTransform> centers = new();
-    [SerializeField] int select;
+    private Vector3 axis;
+    private bool isStarted;
+    private float targetRotate;
+    private List<float> sample;
+    private float sum = 0;
+    private float average = 0;
+    private float timer = 0;
+    private float speed = 0;
+    private int index = 0;
 
     void Awake()
     {
-        foreach (GameObject c in cards)
+        sample = new();
+        axis = clockwise ? Vector3.back : Vector3.forward;
+        int a = Random.Range(12, 15);
+        targetRotate = clockwise ? targetDeg : 360 - targetDeg;
+        targetRotate += 360 * a;
+        Sampling();
+        GetAverage();
+    }
+    void Update()
+    {
+        if (!isStarted) return;
+        if (targetRotate <= 0) return;
+        if (timer <= 0)
         {
-            centers.Add(c.GetComponentInChildren<RectTransform>());
+            timer = average;
+            if (index < samplingCount)
+            {
+                speed = sample[index];
+                index++;
+            }
+            else
+            {
+                speed *= 0.6f; // 샘플링 값 다 썼는데도 안끝날 경우..
+            }
+        }
+        float delta = Time.deltaTime;
+        float temp = timer - delta;
+        float rotate = speed * defaultDPS;
+        if (temp < 0)
+        {
+            rotate *= timer;
+        }
+        else
+        {
+            rotate *= delta;
+        }
+        roulette.Rotate(axis, rotate);
+        targetRotate -= rotate;
+        timer = temp;
+    }
+
+    public void Start()
+    {
+        isStarted = true;
+    }
+
+    private void Sampling()
+    {
+        for (int i = 0; i < samplingCount; i++)
+        {
+            float val = Function((float)i / samplingCount);
+            sum += val;
+            sample.Add(val);
         }
     }
-
-    public void Align()
+    private float Function(float x)
     {
-        float canvWidth = canversTrs.rect.width;
-        float contWidth = contentsTrs.rect.width;
-        float wDelta = contWidth - canvWidth;
-        float canvCenter = canvWidth / 2;
-        float targetPos = centers[select].localPosition.x;
-        float distance = targetPos - canvCenter;
-        float scrollValue = Mathf.Clamp(distance / wDelta, 0, 1);
-        scrollbar.value = scrollValue;
+        return Mathf.Pow(0.002f, 1.2f * x) * 500;
     }
-
-
-
-
+    private void GetAverage()
+    {
+        average = targetRotate / sum / defaultDPS;
+    }
 }
