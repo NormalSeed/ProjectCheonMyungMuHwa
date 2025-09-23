831c49f27b7fe1b4cfbfdeab0f44bcd5316e001f
NormalSeed <dlrjsghk11@naver.com>
2025-09-23 14:45:41 +0900

    [Feat] Stage reset via all heros death
    모든 플레이어 캐릭터가 사망시 스테이지 리셋 기능 구현

--- Assets/PersonalWorkplace/LGH/Scripts/Managers/InGameManager.cs
+++ Assets/PersonalWorkplace/LGH/Scripts/Managers/InGameManager.cs
@@ -1,9 +1,11 @@
+using DG.Tweening;
 using GooglePlayGames.BasicApi;
 using NavMeshPlus.Components;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameManager : MonoBehaviour
 {
@@ -25,6 +27,9 @@ public class InGameManager : MonoBehaviour
     public GameObject alignPoint;
 
     public NavMeshSurface surface;
+
+    [Header("페이드 인/아웃 이미지")]
+    [SerializeField] private Image fadeImage;
 
     [Header("게임 종료 UI")]
     [SerializeField] private GameObject gameQuitUI;
@@ -145,14 +150,16 @@ public class InGameManager : MonoBehaviour
         stageNum++;
     }
 
-    public void CheckAllPlayersDead()
+    public void CheckAllPlayersDead(bool isDead)
     {
         List<PlayerController> players = PartyManager.Instance.players;
 
         // 모든 플레이어가 사망했는지 확인
-        bool allDead = players.All(p => p.isDead);
+        bool allActivePlayersDead = players
+        .Where(p => p.gameObject.activeSelf)
+        .All(p => p.isDead.Value);
 
-        if (allDead)
+        if (allActivePlayersDead)
         {
             StartCoroutine(HandleAllPlayersDead());
         }
@@ -163,40 +170,49 @@ public class InGameManager : MonoBehaviour
         Debug.Log("모든 플레이어 사망. 페이드 아웃 시작");
 
         // 페이드 아웃 효과 추가해야함
+        FadeOut();
 
         yield return new WaitForSeconds(2f); // 암전 시간
 
         // 몬스터 비활성화(풀매니저에서 현재 활성화 상태인 몬스터를 전부 풀로 되돌리는 메서드 추가 요청)
+        PoolManager.Instance.ReleaseAll();
 
         // 스테이지 초기화
         stageProgress = 0;
         monsterDeathStack.Value = 0;
-        alignedNum.Value = 0;
-        isProcessingAlignment = false;
+        stageProgress = 0;
 
         // 페이드인 전에 플레이어 위치 재배치
         for (int i = 0; i < PartyManager.Instance.players.Count; i++)
         {
-            Transform point = alignPoint.transform.Find($"Point{i}");
+            Transform point = alignPoint.transform.Find($"Point{i + 1}");
             if (point != null)
             {
+                Vector3 offset = new Vector3(0, -0.1f, 0);
                 var player = PartyManager.Instance.players[i];
-                var agent = player.GetComponent<UnityEngine.AI.NavMeshAgent>();
+                if (player.gameObject.activeSelf == false) continue;
+
+                var agent = player.NMagent;
 
                 if (agent != null)
                 {
-                    agent.Warp(point.position);
+                    agent.Warp(point.position + offset);
                     player.transform.rotation = point.rotation;
                 }
                 else
                 {
-                    player.transform.position = point.position;
+                    player.transform.position = point.position + offset;
                     player.transform.rotation = point.rotation;
                 }
+            }
+            else
+            {
+                Debug.LogWarning("정렬 포인트가 없음");
             }
         }
 
         // 페이드 인 효과 추가해야함
+        
 
         Debug.Log("스테이지 초기화 완료. 재시작 준비됨");
 
@@ -206,15 +222,34 @@ public class InGameManager : MonoBehaviour
         // 모든 플레이어의 상태를 Move로 설정
         foreach (var player in PartyManager.Instance.players)
         {
+            if (!player.gameObject.activeSelf) continue;
+
+            player.isDead.Value = false;
+            player.spumController.PlayAnimation(PlayerState.IDLE, 0);
+
             var agent = player.BGagent;
             if (agent != null)
             {
+                agent.SetVariableValue("CurState", PlayerStates.Move);
+
                 agent.enabled = false;
                 agent.enabled = true;
                 agent.Init();
-
-                agent.SetVariableValue("CurState", PlayerStates.Move);
             }
         }
+
+        FadeIn();
+    }
+
+    private void FadeOut()
+    {
+        fadeImage.color = new Color(0, 0, 0, 0);
+        fadeImage.DOFade(1f, 2f);
+    }
+
+    private void FadeIn()
+    {
+        fadeImage.color = new Color(0, 0, 0, 1);
+        fadeImage.DOFade(0f, 1.5f);
     }
 }
--- Assets/PersonalWorkplace/LGH/Scripts/PlayerController.cs
+++ Assets/PersonalWorkplace/LGH/Scripts/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour, IDamagable
     public BehaviorGraphAgent BGagent;
 
     public bool movedRight = false;
-    public bool isDead = false;
+    public ObservableProperty<bool> isDead { get; private set; } = new(true);
 
     private Vector3 damageOffest = new Vector3(0, 0.8f, 0);
 
@@ -65,7 +65,7 @@ public class PlayerController : MonoBehaviour, IDamagable
         NMagent.updateRotation = false;
         NMagent.updateUpAxis = false;
 
-        isDead = true;
+        isDead.Value = false;
 
         charID.Subscribe(LoadPlayerData);
 
@@ -103,6 +103,7 @@ public class PlayerController : MonoBehaviour, IDamagable
 
         GameEvents.OnHeroLevelChanged += HandleHeroLevelChanged;
         model.CurHealth.Subscribe(UpdateHealthBar);
+        isDead.Subscribe(InGameManager.Instance.CheckAllPlayersDead);
     }
 
     private void OnDisable()
@@ -116,6 +117,8 @@ public class PlayerController : MonoBehaviour, IDamagable
         skillSet.SetActive(false);
         skillSet = null;
         GameEvents.OnHeroLevelChanged -= HandleHeroLevelChanged;
+        model.CurHealth.UnsubscribeAll();
+        isDead.UnsubscribeAll();
     }
 
     /// <summary>
@@ -278,6 +281,12 @@ public class PlayerController : MonoBehaviour, IDamagable
                 Debug.Log("보호막 지속시간 종료");
             }
         }
+
+        // 테스트용 99999999 데미지 입는 버튼
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            TakeDamage(99999999f);
+        }
     }
 
     /// <summary>
@@ -297,6 +306,8 @@ public class PlayerController : MonoBehaviour, IDamagable
 
     public void TakeDamage(float amount)
     {
+        if (isDead.Value == true) return;
+
         if (isShieldActive)
         {
             shieldAmount -= (float)amount;
@@ -323,13 +334,18 @@ public class PlayerController : MonoBehaviour, IDamagable
 
         DamageText text = DamageTextManager.Instance.Get(transform.position + damageOffest);
         text.SetText(BigCurrency.FromBaseAmount(amount).ToString());
+
+        if (model.CurHealth.Value <= 0f)
+        {
+            Dead();
+        }
     }
 
     public void Dead()
     {
         BGagent.enabled = false;
         spumController.PlayAnimation(PlayerState.DEATH, 0);
-        isDead = true;
+        isDead.Value = true;
     }
 
     public void Heal(float amount)
